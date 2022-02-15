93058dda2a852c9808f71b5b1f39582fdded9af3
abirxlr8 <91585817+abirxlr8@users.noreply.github.com>
2022-02-15 00:46:53 -0500

    Add files via upload

---
+++ Flixster/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  Flixster
+//
+//  Created by Abir T on 2/11/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+}
